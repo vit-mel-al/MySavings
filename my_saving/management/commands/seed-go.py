5327f05a0ff2357d4b06097d28ba3d95183f62f4
Vitaly Melnik <vitali@rarus.ru>
2026-01-12 16:05:20 +0300

    seed add test superuser

--- my_saving/management/commands/seed-go.py
+++ my_saving/management/commands/seed-go.py
@@ -1,7 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 from datetime import datetime
+from django.contrib.auth import get_user_model
 from django_seed import Seed
+import environ
 from ...models import Account, Transaction, Currency, Category
+
+User = get_user_model()
+
+env = environ.Env()
+environ.Env.read_env()
 
 
 class Command(BaseCommand):
@@ -19,8 +26,25 @@ class Command(BaseCommand):
         self.init_data_categories()
         self.stdout.write("- Init \"Categories\" - " + self.style.SUCCESS("Ok"))
 
+        if env('APP_ENV') == 'local':
+            self.add_test_superuser(self)
+
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.stdout.write(self.style.SUCCESS(f"End: {current_time}"))
+
+    @staticmethod
+    def add_test_superuser(self):
+
+        if env('TEST_SUPERUSER_LOGIN'):
+            superuser = User.objects.filter(username=env('TEST_SUPERUSER_LOGIN'))
+            if not superuser.exists():
+                superuser_new = User.objects.create_superuser(
+                    env('TEST_SUPERUSER_LOGIN'),
+                    env('TEST_SUPERUSER_EMAIL'),
+                    env('TEST_SUPERUSER_PASS')
+                )
+                superuser_new.save()
+                self.stdout.write(f"- Init superuser \"{env('TEST_SUPERUSER_LOGIN')}\" - " + self.style.SUCCESS("Ok"))
 
     @staticmethod
     def init_data_currencies():

9059c134a940b05bafa3c7e187f22ccd080169a8
Vitaly Melnik <vitali@rarus.ru>
2026-01-13 19:54:13 +0300

    fix templates

--- my_saving/views.py
+++ my_saving/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Account
 def index(request):
-    return HttpResponse('Hi Vitaly')
+    accounts = Account.objects.all()
+    return render(request, 'main.html', {'accounts': accounts})

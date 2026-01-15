6751514d9ff27e4a6171aa5d221d26a75e315009
Vitaly Melnik <vitali@rarus.ru>
2026-01-15 14:39:31 +0300

    fix

--- my_saving/urls.py
+++ my_saving/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.dashboard, name='dashboard'),
+    path('accounts', views.accounts, name='accounts')
 ]
--- my_saving/views.py
+++ my_saving/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Account
-def index(request):
-    accounts = Account.objects.all()
-    return render(request, 'my_saving/dashboard.html', {'accounts': accounts})
+def dashboard(request):
+    return render(request, 'my_saving/dashboard.html')
+
+
+def accounts(request):
+    accounts_list = Account.objects.all()
+    return render(request, 'my_saving/accounts.html', {'accounts': accounts_list})

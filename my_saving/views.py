f125e682509fcccabd4477a6c3176be3be9d39f5
Vitaly Melnik <vitali@rarus.ru>
2026-01-16 17:47:21 +0300

    fix

--- my_saving/views.py
+++ my_saving/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Account, Category
-
 from .forms import CreateUserForm, LoginForm
-
 from django.contrib.auth.decorators import login_required
-
-# - Authentication models and functions
-
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate, login, logout
 
@@ -16,35 +10,39 @@ def index(request):
     return render(request, 'my_saving/index.html')
 
 
+@login_required(login_url="user_login")
 def dashboard(request):
     return render(request, 'my_saving/dashboard.html')
 
 
+@login_required(login_url="user_login")
 def my_savings(request):
     accounts_list = Account.objects.all()
-    return render(request, 'my_saving/accounts.html', {'accounts': accounts_list})
+    context = {'accounts': accounts_list}
+    return render(request, 'my_saving/accounts.html', context=context)
 
 
+@login_required(login_url="user_login")
 def accounts(request):
     accounts_list = Account.objects.all()
-    return render(request, 'my_saving/accounts.html', {'accounts': accounts_list})
+    context = {'accounts': accounts_list}
+    return render(request, 'my_saving/accounts.html', context=context)
 
 
+@login_required(login_url="user_login")
 def categories(request):
     categories_list = Category.objects.all()
-    return render(request, 'my_saving/categories.html', {'categories_list': categories_list})
+    context = {'categories_list': categories_list}
+    return render(request, 'my_saving/categories.html', context=context)
 
 
 def user_register(request):
     form = CreateUserForm()
 
     if request.method == "POST":
-
         form = CreateUserForm(request.POST)
-
         if form.is_valid():
             form.save()
-
             return redirect("my-login")
 
     context = {'register_form': form}
